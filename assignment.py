85b489d694793e948f950fe3795e0c703309b2a8
ryanpalatty <ryan.palatty@stu.ocsb.ca>
2024-11-28 23:42:31 +0000

    python fix

--- assignment.py
+++ assignment.py
@@ -1,7 +1,11 @@
-iterations = int(input("Enter a number: "))
-pi = 0
 
-for i in range(iterations):
-    pi += (-1)**i / (2 * i + 1)
+iterations = input()
 
-print("Approximated value of PI: ", 4 * pi)
+if iterations.isdigit() and int(iterations) > 0:
+    iterations = int(iterations)
+    pi = 0
+    for i in range(iterations):
+        pi += (-1)**i / (2 * i + 1)
+    print(4 * pi)
+else:
+    print("Error")
